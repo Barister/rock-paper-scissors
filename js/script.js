a8f2cec0d0fb9a19188d48f62620831abfb66843
Barister <59793749+Barister@users.noreply.github.com>
2023-01-26 17:28:25 +0300

    First success realisation to play game 5 times

--- js/script.js
+++ js/script.js
@@ -1,48 +1,68 @@
-// Rules: Rock beats scissors, scissors beats paper, paper beats rock.
+//to make variables
+let playerChoice;
+let computerChoice;
 
-//ПРописать правила игры и переменные.
- //   КОнстанты: игрок, компьютер; камень, ножницы, бумага, и кто кого бьет
+let paper = "Paper";
+let rock = "Rock";
+let scissors = "Scissors";
 
-// Перменные: выбор игрока, выбор компьютера
-    let playerChoice;
-    let computerChoice;
-
-    let paper = "paper";
-    let rock = "rock";
-    let scissors = "scissors";
-
-    let elements = [paper, rock, scissors];
+let elements = [paper, rock, scissors];
  
-//Получить от игрока выбор предмета, которым он будет играть. Предложить варианты, проверить что выбор соответствует вариантам
-    function getPlayerChoice() {
-        playerChoice = scissors;
-        
-    }
-//ПОлучить от компьютера выбор предмета, которым он будет играть. Сгенерировть случайный выбор из трех предметов.
-    function getComputerChoice() {
-        computerChoice = elements[Math.floor(Math.random() * 3)];
-    }
+//to make function for get PlayerChoice and validate choise
 
+function getPlayerChoice() {
+    playerChoice = window.prompt("Please choose your weapon! Paper, rock or scissors?");
+    playerChoice = playerChoice[0].toUpperCase() + playerChoice.substring(1);
+    while (!elements.includes(playerChoice)) {
+        console.log("wrongChoice: ", playerChoice);
+        playerChoice = window.prompt("You chose the wrong weapon! Please paper, rock or scissors?");
+        playerChoice = playerChoice[0].toUpperCase() + playerChoice.substring(1);
+    }
+    return playerChoice;
+}
+
+//to to make function for get computerChoice randomly
+
+function getComputerChoice() {
+    computerChoice = elements[Math.floor(Math.random() * 3)];
+}
+
+
+//to make a function to play one game: 
+
+function playSingleRound() {
+    let result;
+    //execution to get rivals choices; 
     getPlayerChoice();
     console.log("playerChoice: ", playerChoice);
 
     getComputerChoice();    
     console.log("computerChoice: ", computerChoice);
-
-// Сравнить предметы игрока и компьютера. Объявить победителя либо объявить ничью.
-    function playSingleRound(playerChoice, computerChoice) {
-        let result;
-        if (playerChoice == computerChoice) {
-            result = "Ничья!!!";
-        }
-        else if ((playerChoice == paper && computerChoice == rock) || (playerChoice == rock && computerChoice == scissors) || (playerChoice == scissors && computerChoice == paper)) {
-            result = "Congratulations! You win!!!"
-        }
-        else {
-            result = "Oh, no! Computer win!!!"
-        }
-        console.log(result);
+    //  to compare competitor choices
+    if (playerChoice == computerChoice) {
+        result = "Draw!!!";
     }
+    else if ((playerChoice == paper && computerChoice == rock) || (playerChoice == rock && computerChoice == scissors) || (playerChoice == scissors && computerChoice == paper)) {
+        result = `Congratulations!  ${playerChoice} beats ${computerChoice}. You win!!!`;
+    }
+    else {
+        result = `Oh, no! ${computerChoice} beats ${playerChoice}. Computer win!!! `;
+    }
+    console.log(result);
+    return result;
+}
 
-    playSingleRound(playerChoice, computerChoice);
+//to make a func to play 5 times;
+
+function game(times){
+    for (let i = 0; i < times; i++) {
+        playSingleRound();
+        console.log("i:", i);
+    }
     
+    
+}
+
+//execution of func to play a game 5 times
+
+game(5);
